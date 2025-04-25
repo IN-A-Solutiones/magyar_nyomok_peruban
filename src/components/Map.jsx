1648ad8e1f371d8a35beebcfe04ce5b2c36c1837
Tamas <tamas.fehertoi@gmail.com>
2025-04-25 09:44:41 -0500

    feat: Add addresses to map locations in multiple languages
    - Add address field to all locations in Hungarian, Spanish and Quechua
    - Display addresses in map popups with italic styling
    - Update popup layout to show name, address and description

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,39 +1,95 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
+import { useTranslation } from "react-i18next";
 
 const locations = [
   {
     id: 1,
-    name: "Falfestmény Aguas Calientesben",
+    name: {
+      hu: "Falfestmény Aguas Calientesben",
+      es: "Mural en Aguas Calientes",
+      qu: "Pintura mural Aguas Calientespi",
+    },
+    address: {
+      hu: "Aguas Calientes, Cusco régió, Peru",
+      es: "Aguas Calientes, Región Cusco, Perú",
+      qu: "Aguas Calientes, Cusco suyu, Piruw",
+    },
     lat: -13.1588,
     lng: -72.5314,
-    description: "Magyar utalás egy falfestményen az inkák városánál.",
+    description: {
+      hu: "Magyar utalás egy falfestményen az inkák városánál.",
+      es: "Referencia húngara en un mural cerca de la ciudad de los incas.",
+      qu: "Hungaruyuq rimay muralpi, inka llaqtap qayllanpi.",
+    },
   },
   {
     id: 2,
-    name: "Ébneth Lajos szobrai – Lima",
+    name: {
+      hu: "Ébneth Lajos szobrai – Lima",
+      es: "Esculturas de Ébneth Lajos – Lima",
+      qu: "Ébneth Lajospa esculturas – Lima",
+    },
+    address: {
+      hu: "Parque Salazar, Miraflores, Lima, Peru",
+      es: "Parque Salazar, Miraflores, Lima, Perú",
+      qu: "Salazar parque, Miraflores, Lima, Piruw",
+    },
     lat: -12.125,
     lng: -77.0344,
-    description: "Szobrok a Miraflores-i Salazar parkban.",
+    description: {
+      hu: "Szobrok a Miraflores-i Salazar parkban.",
+      es: "Esculturas en el Parque Salazar de Miraflores.",
+      qu: "Esculturas Miraflores Salazar parquepi.",
+    },
   },
   {
     id: 3,
-    name: "Katedrális – Lima",
+    name: {
+      hu: "Katedrális – Lima",
+      es: "Catedral – Lima",
+      qu: "Catedral – Lima",
+    },
+    address: {
+      hu: "Plaza Mayor, Lima történelmi központja, Peru",
+      es: "Plaza Mayor, Centro Histórico de Lima, Perú",
+      qu: "Plaza Mayor, Lima historico centro, Piruw",
+    },
     lat: -12.0464,
     lng: -77.0301,
-    description: "A főtér katedrálisa, magyar vonatkozás említve.",
+    description: {
+      hu: "A főtér katedrálisa, magyar vonatkozás említve.",
+      es: "La catedral en la Plaza Mayor, con referencia húngara.",
+      qu: "Plaza Mayorpi catedral, hungaruyuq rimaywan.",
+    },
   },
   {
     id: 4,
-    name: "Petőfi tér – La Paz",
+    name: {
+      hu: "Petőfi tér – La Paz",
+      es: "Plaza Petőfi – La Paz",
+      qu: "Petőfi plaza – La Paz",
+    },
+    address: {
+      hu: "La Paz, Bolívia",
+      es: "La Paz, Bolivia",
+      qu: "La Paz, Wuliwya",
+    },
     lat: -16.5,
     lng: -68.15,
-    description: "Tér, amelyet Petőfi Sándorról neveztek el Bolíviában.",
+    description: {
+      hu: "Tér, amelyet Petőfi Sándorról neveztek el Bolíviában.",
+      es: "Plaza nombrada en honor al poeta húngaro Sándor Petőfi en Bolivia.",
+      qu: "Sutichasqa hungaruyuq harawiq Sándor Petőfip sutin, Wuliwyapi.",
+    },
   },
 ];
 
 export default function Map() {
+  const { t, i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+
   return (
     <div className="map-container">
       <MapContainer
@@ -48,9 +104,11 @@ export default function Map() {
         {locations.map((loc) => (
           <Marker key={loc.id} position={[loc.lat, loc.lng]}>
             <Popup>
-              <strong>{loc.name}</strong>
+              <strong>{loc.name[currentLanguage]}</strong>
               <br />
-              {loc.description}
+              <em>{loc.address[currentLanguage]}</em>
+              <br />
+              {loc.description[currentLanguage]}
             </Popup>
           </Marker>
         ))}
