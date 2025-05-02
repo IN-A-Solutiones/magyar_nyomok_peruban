eb1494b041458f98c6cb559c1b8fbba5df7918ea
Tamas <tamas.fehertoi@gmail.com>
2025-05-02 17:59:48 -0500

    feat: add custom marker icons to map
    - Add custom marker icons from public/leaflet directory
    - Configure marker icon with retina support
    - Update marker icon settings (size, anchor points)

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,6 +3,18 @@ import "leaflet/dist/leaflet.css";
 import "./Map.css";
 import { useTranslation } from "react-i18next";
 import { locations } from "../data/locations";
+import L from "leaflet";
+
+// Define the marker icon
+const markerIcon = new L.Icon({
+  iconUrl: "/leaflet/marker-icon.png",
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+  shadowUrl: "/leaflet/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 export default function Map() {
   const { t, i18n } = useTranslation();
@@ -23,6 +35,7 @@ export default function Map() {
           <Marker
             key={loc.id}
             position={[loc.coordinates.lat, loc.coordinates.lng]}
+            icon={markerIcon}
           >
             <Popup>
               <strong>{loc.title[currentLanguage]}</strong>
